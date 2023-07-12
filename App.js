8cacc37a2d0719840f53ff361a0d7378f6faada0
gracebir <grace.devolop@gmail.com>
2023-07-12 23:10:31 +0200

    first commmit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
+}
+// lwammekonen28@gmail.com, Lwam Mekonen
+
