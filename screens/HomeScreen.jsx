10309df63ecb4735eccc744a32c86172cfa10a0f
gracebir <grace.devolop@gmail.com>
2023-07-12 18:01:40 +0200

    home page

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View className="justify-center items-center flex-1">
+      <Text className="text-red-400 text-center">HomeScreen</Text>
+    </View>
+  )
+}
